3ce8117da28b3fd26932b11042e6ddeb40946f21
Ankit Charpe <ankitcharpe01@gmail.com>
2025-01-07 10:48:37 +0530

    Addition to 5+5

--- Calculator.java
+++ Calculator.java
@@ -9,46 +9,8 @@ public class Calculator {
 	
 	public static void main(String[] args)
 	{
-				double num1, num2;
+             System.out.println("HELLO");
+             System.out.println(5+5);
 
-				Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter the numbers:");
-
-				num1 = sc.nextDouble();
-		num2 = sc.nextDouble();
-
-		System.out.println("Enter the operator (+,-,*,/):");
-
-		char op = sc.next().charAt(0);
-		double o = 0;
-
-		switch (op) {
-				case '+':
-			o = num1 + num2;
-			break;
-
-				case '-':
-			o = num1 - num2;
-			break;
-
-				case '*':
-			o = num1 * num2;
-			break;
-
-				case '/':
-			o = num1 / num2;
-			break;
-
-		default:
-			System.out.println("You enter wrong input");
-		}
-
-		System.out.println("The final result:");
-		System.out.println();
-
-		// print the final result
-		System.out.println(num1 + " " + op + " " + num2
-						+ " = " + o);
 	}
 }
