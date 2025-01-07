3fc96acfe1c4ee114dbe66592142bbc2ae20ac7d
Ankit Charpe <ankitcharpe01@gmail.com>
2025-01-07 12:28:11 +0530

    Addition functionality added

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,4 @@
-// Java program for simple calculator
-import java.io.*;
-import java.lang.*;
-import java.lang.Math;
-import java.util.Scanner;
 
-// Driver class
 public class Calculator {
 	
 	public static void main(String[] args)
--- Ucalculator.java
+++ Ucalculator.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
-public class Ucalculator {
-	
-	public static void main(String[] args)
-	{
-            System.out.pritlln ("Welcome to the calculator");	
-	}
+public class Ucalculator{
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Welcome to Calculator.");
+                System.out.print("Enter first number: ");
+                double num1 = sc.nextDouble();
+                System.out.print("Enter second number: ");
+                double num2 = sc.nextDouble();
+
+                double result = addition(num1, num2);
+                System.out.println("The addition of two nos is: " + result);
+
+                sc.close();
+        }
+
+        public static double addition(double num1, double num2) {
+                return num1 + num2;
+        }
 }
