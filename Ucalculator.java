594b935ad4c23686c0341ce2df2d6ab23e17f0c8
Ankit Charpe <ankitcharpe01@gmail.com>
2025-01-07 12:51:42 +0530

    Substraction

--- Ucalculator.java
+++ Ucalculator.java
@@ -9,8 +9,22 @@ public class Ucalculator{
                 System.out.print("Enter second number: ");
                 double num2 = sc.nextDouble();
 
+		 System.out.println("Enter Choice");
+		 System.out.println("1.Add");
+		 System.out.println("2.Subtract");
+		 int choice = sc.nextInt();
+		
+		
+		switch(choice){
+		case 1:
                 double result = addition(num1, num2);
                 System.out.println("The addition of two nos is: " + result);
+		break;
+                
+		case 2:
+                double result = subtract(num1,num2);
+		System.out.println("The substraction of two nos is: " + result);
+		break;
 
                 sc.close();
         }
@@ -18,4 +32,9 @@ public class Ucalculator{
         public static double addition(double num1, double num2) {
                 return num1 + num2;
         }
+	
+	public static double subtract(double num1, double num2) {
+                return num1 - num2;
+        }
+
 }
